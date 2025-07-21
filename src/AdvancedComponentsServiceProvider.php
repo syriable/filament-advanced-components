20cedd7281cf3d0bc71a2bf41b5b53d0a2635618
Ghiath <syriable@gamil.com>
2025-07-21 20:08:10 +0200

    feat: add macro counter to Textarea component & fix macro hiddenHeader

--- src/AdvancedComponentsServiceProvider.php
+++ src/AdvancedComponentsServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace Syriable\FilamentAdvancedComponents;
 
+use Filament\Actions\Action;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Components\Wizard;
 use Filament\Support\Assets\Css;
 use Filament\Support\Facades\FilamentAsset;
@@ -29,6 +33,34 @@ class AdvancedComponentsServiceProvider extends PackageServiceProvider
         ], 'syriable/filament-advanced-components');
 
 
-        Wizard::macro('heddinHeader',$this->extraAttributes(['class' => 'wizard-hidden-header']));
+        Wizard::macro('hiddenHeader',fn() => $this->extraAttributes(['class' => 'wizard-hidden-header']));
+
+        Textarea::macro('counter', fn () => $this->fieldWrapperView('filament-advanced-components::components.filament.textarea')->extraAlpineAttributes(['x-init' => '$watch(\'state\',value => length = value?.length); length = state?.length ?? length']));
+
+        Toggle::macro(
+            'confirmation',
+            fn () => $this->live()
+                ->afterStateUpdated(function ($component, $livewire) {
+                    $livewire->mountFormComponentAction($component->getStatePath(), 'notify_users_confirmation');
+                })
+                ->registerActions([
+                    Action::make('notify_users_confirmation')
+                        ->requiresConfirmation()
+                        ->modalHeading(fn ($component) => $component->getLabel())
+                        ->modalAlignment('start')
+                        ->modalCancelAction(false)
+                        ->modalSubmitAction(false)
+                        ->closeModalByClickingAway(false)
+                        ->closeModalByEscaping(false)
+                        ->modalIconColor('danger')
+                        ->modalCloseButton(false)
+                        ->extraModalFooterActions(fn (Action $action): array => [$action->makeModalSubmitAction('yes', arguments: ['submit' => true])->icon('heroicon-m-check')->color('success'), $action->makeModalSubmitAction('no', arguments: ['cancel' => true])->icon('heroicon-m-x-mark')->color('danger')])
+                        ->action(function (array $arguments, Set $set, $component) {
+                            if (data_get($arguments, 'cancel', false)) {
+                                $set($component->getName(), ! $component->getState());
+                            }
+                        }),
+                ]),
+        );
     }
 }
